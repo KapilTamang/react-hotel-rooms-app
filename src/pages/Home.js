fccf75169d5cd1e1c94bbd6e4c4a947178700390
Kapil Tamang <tamang.kapil11235@gmail.com>
2021-04-24 13:46:01 +0545

    home page modification

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,15 @@ import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
+import bgVid from '../videos/bgVid.mp4';
 
 const Home = () => {
     return (
         <>
             <Hero hero="defaultHero">
+                <div className="bgVidContainer">
+                    <video src={bgVid} autoPlay loop muted type="video/mp4" />
+                </div>
                 <Banner
                     title="Luxurious Rooms"
                     subtitle="Deluxe rooms starting from $200.">
